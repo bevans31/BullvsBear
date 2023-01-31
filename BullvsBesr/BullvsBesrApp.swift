d11973eefc2a1220388fec17f35c760e50966f93
Bran <brandonevan799@gmail.com>
2023-01-31 10:08:43 -0500

    Initial Commit

---
+++ BullvsBesr/BullvsBesrApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullvsBesrApp.swift
+//  BullvsBesr
+//
+//  Created by Brandon Evans on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct BullvsBesrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
