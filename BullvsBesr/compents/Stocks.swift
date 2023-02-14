28bd0081e5ee3c000272fceb80aabab107d38ba4
Bran <brandonevan799@gmail.com>
2023-02-14 11:54:35 -0500

    extension page

--- BullvsBesr/compents/Stocks.swift
+++ BullvsBesr/compents/Stocks.swift
@@ -13,3 +13,5 @@ struct Stocks {
     var tickerName = [ "APPL", "TSLA", "VZ","WMT", "EA", "LYFT","BAC", "MCD", "GOOG","DAL","META","SBUX"]
     var stockPrice =  [ "153.85","194.64","40.32", "145.91", "113.20","10.46", "35.65", "266.20","94.61","38.60","179.43","108.52"]
 }
+
+
--- BullvsBesr/views/GamePage.swift
+++ BullvsBesr/views/GamePage.swift
@@ -30,35 +30,26 @@ struct GamePage: View {
             }
 
             Spacer()
-            TabView(selection: self.$index) {
-                ForEach(0 ... t.name.count-1, id: \.self) { tt in
+           // TabView(selection: self.$index) {
+                //9ForEach(0 ... t.name.count-1, id: \.self) { tt in
                     
                     VStack {
                         Text(t.name[0])
-                            .font(.title)
-                            .bold()
-                            .padding()
+                            .stockTitle()
                         ZStack{
                             Rectangle()
                                 .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
                                 .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
                                 .cornerRadius(/*@START_MENU_TOKEN@*/5.0/*@END_MENU_TOKEN@*/)
-                                
-                            
+                    
                             Image(t.logo[0])
-                                .resizable()
-                                .frame(width: 180.0, height: 200.0)
-                                .padding(.horizontal)
-                                .scaledToFit()
-                         
-                            
-                            
+                                .logoFormat()
                         }
                     }
                     
                     .padding()
-                }
-            }
+               // }
+            //}
             .frame(height: 300)
             HStack{
                 Text(t.tickerName[0])
@@ -68,42 +59,15 @@ struct GamePage: View {
                 Text(t.stockPrice[0])
                     .bold()
                     .padding()
-                   
-                    
             }
-           
             HStack{
                 Button("INVEST"){
-                 
-                }
-                .padding(.horizontal, 2.0)
-                .frame(maxWidth: 100, maxHeight: 80)
-                .contentShape(Rectangle())
-                
-                .foregroundColor(/*@START_MENU_TOKEN@*/Color.white/*@END_MENU_TOKEN@*/)
-                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.green/*@END_MENU_TOKEN@*/)
-                .cornerRadius(10)
-                .shadow(radius: 0)
-                .font(.system(size: 22))
-                
+                } .investFormat()
                    Spacer()
                 Button("PASS"){
-                    
                 }
-                .padding(.horizontal, 2.0)
-                .frame(maxWidth: 100, maxHeight: 80)
-                .contentShape(Rectangle())
-                
-                .foregroundColor(/*@START_MENU_TOKEN@*/Color.white/*@END_MENU_TOKEN@*/)
-                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.red/*@END_MENU_TOKEN@*/)
-                .cornerRadius(10)
-                .shadow(radius: 0)
-                .font(.system(size: 22))
-                
-
-                
+                .passFormat()
             }
-            
             Spacer()
         }
        .onReceive(timer) { time in
